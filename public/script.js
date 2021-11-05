32504ce75cce2b5cd0ef4cbc344d284918b7722e
Gizem Güler <fgizemguler@gmail.com>
2021-11-05 11:11:18 +0300

    zoom clone

---
+++ public/script.js
@@ -0,0 +1,166 @@
+const socket = io("/");
+const videoGrid = document.getElementById("video-grid");
+const myVideo = document.createElement("video");
+myVideo.muted = true;
+
+var peer = new Peer(undefined, {
+  path: "/peerjs",
+  host: "/",
+  port: "3030",
+});
+
+
+let myVideoStream;
+const peers = {};
+navigator.mediaDevices
+  .getUserMedia({
+    video: true,
+    audio: true,
+  })
+  .then((stream) => {
+    myVideoStream = stream;
+    addVideoStream(myVideo, stream);
+
+    peer.on("call", (call) => {
+      call.answer(stream);
+      const video = document.createElement("video");
+      call.on("stream", (userVideoStream) => {
+        addVideoStream(video, userVideoStream);
+      });
+    });
+
+    socket.on('user-connected', userId => {
+      // user is joining`
+      setTimeout(() => {
+      // user joined
+      connectToNewUser(userId, stream)
+      }, 1000)
+      })  
+
+    let text = $("input");
+    console.log(text);
+
+    $("html").keydown((e) => {
+      if (e.which == 13 && text.val().length !== 0) {
+        console.log(text.val());
+        socket.emit("message", text.val());
+        text.val("");
+      }
+    });
+
+    socket.on("createMessage", (message) => {
+      console.log("s", message);
+      $("ul").append(`<li class="message"><b>user</b><br/>${message}</li>`);
+      scrollToBottom();
+    });
+  });
+
+peer.on("open", (id) => {
+  socket.emit("join-room", ROOM_ID, id);
+});
+
+socket.on("user-disconnected", (userId) => {
+  if (peers[userId]) peers[userId].close();
+});
+
+const connectToNewUser = (userId, stream) => {
+  const call = peer.call(userId, stream);
+  const video = document.createElement("video");
+  call.on("stream", (userVideoStream) => {
+    addVideoStream(video, userVideoStream);
+  });
+  call.on("close", () => {
+    video.remove();
+  });
+  peers[userId] = call;
+};
+
+const addVideoStream = (video, stream) => {
+  video.srcObject = stream;
+  video.addEventListener("loadedmetadata", () => {
+    video.play();
+  });
+  videoGrid.append(video);
+};
+
+const scrollToBottom = () => {
+  var d = $(".main__chat_window");
+  d.scrollTop(d.prop("scrollHeight"));
+};
+
+const muteUnmute = () => {
+  const enabled = myVideoStream.getAudioTracks()[0].enabled;
+  if (enabled) {
+    myVideoStream.getAudioTracks()[0].enabled = false;
+    setUnmuteButton();
+  } else {
+    setMuteButton();
+    myVideoStream.getAudioTracks()[0].enabled = true;
+  }
+};
+
+const playStop = () => {
+  const enabled = myVideoStream.getVideoTracks()[0].enabled;
+  if (enabled) {
+    myVideoStream.getVideoTracks()[0].enabled = false;
+    setPlayVideo();
+  } else {
+    setStopVideo();
+    myVideoStream.getVideoTracks()[0].enabled = true;
+  }
+};
+
+const setMuteButton = () => {
+  const html = `
+    <i class="fas fa-microphone"></i>
+    <span>Mute</span>
+  `;
+  document.querySelector(".main__mute_button").innerHTML = html;
+};
+
+const setUnmuteButton = () => {
+  const html = `
+    <i class="unmute fas fa-microphone-slash"></i>
+    <span>Unmute</span>
+  `;
+  document.querySelector(".main__mute_button").innerHTML = html;
+};
+
+const setStopVideo = () => {
+  const html = `
+    <i class="fas fa-video"></i>
+    <span>Stop Video</span>
+  `;
+  document.querySelector(".main__video_button").innerHTML = html;
+};
+
+const setPlayVideo = () => {
+  const html = `
+  <i class="stop fas fa-video-slash"></i>
+    <span>Play Video</span>
+  `;
+  document.querySelector(".main__video_button").innerHTML = html;
+};
+
+let navOpen = true;
+
+function openCloseNav() {
+  if (navOpen) {
+    styleNav("none", "1");
+  } else {
+    styleNav("flex", "0.8");
+  }
+  navOpen = !navOpen;
+}
+
+function styleNav(display, flex) {
+  var elems0 = document.getElementsByClassName("main__right");
+  for (var i = 0; i < elems0.length; i += 1) {
+    elems0[i].style.display = `${display}`;
+  }
+
+  var elems1 = document.getElementsByClassName("main__left");
+  for (var i = 0; i < elems1.length; i += 1) {
+    elems1[i].style.flex = `${flex}`;
+  }
+}
